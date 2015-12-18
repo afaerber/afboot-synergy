fc8553402194e189f607415fb45636e3d8d33dc8
Andreas Färber <andreas.faerber@web.de>
2015-12-18 22:22:59 +0100

    Initial draft
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>

---
+++ Makefile
@@ -0,0 +1,31 @@
+CROSS_COMPILE ?= arm-none-eabi-
+
+CC = $(CROSS_COMPILE)gcc
+OBJCOPY = $(CROSS_COMPILE)objcopy
+OBJDUMP = $(CROSS_COMPILE)objdump
+SIZE = $(CROSS_COMPILE)size
+GDB = $(CROSS_COMPILE)gdb
+OPENOCD = openocd
+
+CFLAGS := -mthumb -mcpu=cortex-m4
+CFLAGS += -ffunction-sections -fdata-sections
+CFLAGS += -Os -std=gnu99 -Wall
+LDFLAGS := -nostartfiles -Wl,--gc-sections
+
+obj-y += main.o
+
+all: dk-s7g2.elf dk-s7g2
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $< -o $@
+
+dk-s7g2.elf: $(obj-y) Makefile
+	$(CC) -T s7g2.lds $(LDFLAGS) -o dk-s7g2.elf $(obj-y)
+
+dk-s7g2: dk-s7g2.elf Makefile
+	$(OBJCOPY) -Obinary dk-s7g2.elf dk-s7g2.bin
+	$(OBJDUMP) -S dk-s7g2.elf > dk-s7g2.lst
+	$(SIZE) dk-s7g2.elf
+
+clean:
+	@rm -f *.o *.elf *.bin *.lst
---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void)
+{
+
+	while (1) {
+	}
+
+	return 0;
+}
+
+static void noop(void)
+{
+	while (1) {
+	}
+}
+
+extern unsigned int _end_text;
+extern unsigned int _start_data;
+extern unsigned int _end_data;
+extern unsigned int _start_bss;
+extern unsigned int _end_bss;
+
+void reset(void);
+
+void reset(void)
+{
+	unsigned int *src, *dst;
+
+	src = &_end_text;
+	dst = &_start_data;
+	while (dst < &_end_data) {
+		*dst++ = *src++;
+	}
+
+	dst = &_start_bss;
+	while (dst < &_end_bss) {
+		*dst++ = 0;
+	}
+
+	main();
+}
+
+extern unsigned long _stack_top;
+
+__attribute__((section(".vector_table")))
+void (*vector_table[16 + 96])(void) = {
+	(void (*))&_stack_top,
+	reset,
+	noop,
+	noop,
+	noop,
+	noop,
+	noop,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	noop,
+	noop,
+	NULL,
+	noop,
+	noop,
+	[16 ... 16 + 96 - 1] = noop,
+};
