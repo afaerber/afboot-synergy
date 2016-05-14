8e0a26ba3e3750870b311e7de3fe1bd0da9e1854
Andreas Färber <andreas.faerber@web.de>
2016-05-15 01:39:33 +0200

    Blink DK-S7G2 LEDs
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>

--- main.c
+++ main.c
@@ -1,10 +1,34 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define PORTx_PCNTR1(x)	(0x40040000UL + (x) * 0x20)
+#define PORTx_PCNTR3(x)	(0x40040008UL + (x) * 0x20)
+
+static void delay(uint32_t timeout)
+{
+	uint32_t i;
+
+	for (i = 0; i < timeout; i++) {
+		asm volatile ("nop; nop; nop; nop");
+	}
+}
+
 int main(void)
 {
+	volatile uint32_t *PORT8_PCNTR1 = (void*)PORTx_PCNTR1(8);
+	volatile uint32_t *PORT8_PCNTR3 = (void*)PORTx_PCNTR3(8);
+	uint32_t val;
+
+	val = *PORT8_PCNTR1;
+	val |=    (1 << 10) | (1 << 9) | (1 << 8) | (1 << 7);
+	val &= ~(((1 << 10) | (1 << 9) | (1 << 8) | (1 << 7)) << 16);
+	*PORT8_PCNTR1 = val;
 
 	while (1) {
+		*PORT8_PCNTR3 = (((1 <<  9) | (1 << 8)) << 16) | (1 << 10) | (1 << 7);
+		delay(100000);
+		*PORT8_PCNTR3 = (((1 << 10) | (1 << 7)) << 16) | (1 <<  9) | (1 << 8);
+		delay(100000);
 	}
 
 	return 0;
